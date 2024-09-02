a19555ccef61a275416e2eb13b4b037c7f5c965c
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-09-02 09:53:25 -0300

    oi

---
+++ app/tela-signup/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
+
+export default telaSignup = () => {
+
+return <View style={style.container}>
+<View>
+<Pressable
+style={style.botao}
+
+>
+</Pressable>
+</View>
+</View>
+}
