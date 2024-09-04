3cbe833f7a96e7ca3d28c4cad86875998282edbf
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-09-04 09:55:49 -0300

    commit

--- app/tela-signup/index.jsx
+++ app/tela-signup/index.jsx
@@ -2,102 +2,98 @@ import React, { useState } from 'react';
 import { Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
 
 export default telaSignup = () => {
-        const [nome, setNome] = useState('ramon b');
-        const [email, setEmail] = useState('ramon.brignoli@edu.sc.senai.br');
-        const [senha, setSenha] = useState('senhasupersegura');
-      
-        const handleSignup = async () => {
-            const user = {
-              nome: nome,
-              email: email,
-              senha: senha,
-            };
-        
-            try {
-              const response = await fetch('https://taskhub-s37f.onrender.com/auth/signup', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-              });
-        
-              if (response.ok) {
-                const data = await response.json();
-                console.log('Signup successful', data);
-              } else {
-                const errorData = await response.json();
-                console.error('Signup failed', errorData);
-              }
-            } catch (error) {
-              console.error('Error during signup', error);
-            }
-          };
+  const [nome, setNome] = useState('ramon b');
+  const [email, setEmail] = useState('ramon.brignoli@edu.sc.senai.br');
+  const [senha, setSenha] = useState('senhasupersegura');
 
-        return (
-            <View style={styles.container}>
-              <Text style={styles.title}>Sign Up</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Nome"
-                value={nome}
-                onChangeText={setNome}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Senha"
-                secureTextEntry
-                value={senha}
-                onChangeText={setSenha}
-              />
-                 <Pressable style={styles.button} onPress={handleSignup}>
-                 <Text style={styles.buttonText}>Sign Up</Text>
-                 </Pressable>
-            </View>
-          );
-        };
-        
-        const styles = StyleSheet.create({
-          container: {
-            flex: 1,
-            padding: 16,
-            alignItems: 'center',
-            justifyContent: 'center',
-          },
+  const handleSignup = async () => {
+    try {
+      const response = await fetch('https://taskhub-s37f.onrender.com/auth/signup', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "name":nome,
+          "email":email,
+          "password":senha
+        }),
+      })
+        .then((res)=>{if(res==200) allert('Usuário cadastrado com sucesso!')})
 
-          title: {
-            fontSize: 24,
-            marginBottom: 16,
-            textAlign: 'center',
-          },
-          input: {
-            backgroundColor: '#f9fafb',
-            borderWidth: 2,
-            borderRadius: 10,
-            borderColor: '#001d6e',
-            height: 50,
-            width: 230,
-            borderColor: '#ccc',
-            borderWidth: 1,
-            marginBottom: 12,
-            paddingHorizontal: 8,
-          },
+    } catch (error) {
+      console.error('Error during signup', error);
+    }
+  };
 
-          button: {
-            width: 230,
-            height: 58,
-            borderRadius: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#1f2937',
-            borderWidth: 2,
-            borderRadius: 10,
-            borderColor: '#1f2937',
-          },
-        });
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        onChangeText={setSenha}
+      />
+      <Pressable style={styles.button} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: '#f9fafb',
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#001d6e',
+    height: 50,
+    width: 230,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+
+  button: {
+    width: 230,
+    height: 58,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1f2937',
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#1f2937',
+  },
+  buttonText: {
+    color: '#ffffff',
+  }
+});
