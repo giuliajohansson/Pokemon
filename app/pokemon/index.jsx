35566da5a3c198ad3b102eeda0666ea3eec21dc8
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-09-09 09:33:30 -0300

    pokemon

--- app/pokemon/index.jsx
+++ app/pokemon/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { View, Text, Stylesheet, Picker } from 'react-native'
+import {Picker} from '@react.native.picker'
 import styles from './styles.jsx'
 
 
@@ -7,30 +8,33 @@ import styles from './styles.jsx'
 const Pokemon = () => {
     const [pokemon, setPokemon] = useState('')
 
-    const lista_pokemons = [
-        {nome:'Charizard', id:1},
-        {nome:'Blastoise', id:2},
-        {nome:'Charizard', id:3}
-    ]
+//    const lista_pokemons = [
+//        {nome:'Charizard', id:1},
+//        {nome:'Blastoise', id:2},
+//        {nome:'Charizard', id:3}
+//    ]
 
-    useEffect(() => {
+   useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon')
-        .then(resposta => resposta.json)
-
+        .then(resposta => resposta.json())
+        .then(dados => setListaPokemons(dados.reults))
+        .catch(console.log('Aconteceu um erro'))
     })
 
     return (
         <View>
         <Text>Selecione</Text>
         <Picker
+            style={stylers.pickerInput}
             selectedValue={pokemon}
             onValueChange={(item) => setPokemon(item)}
         >
+            <Picker.item labem={'Selecione um Pokémon'}/>
             {lista_pokemons.map((item, index) => (
                 <Picker.Item key={index} label={item.name} value={item.url} />
             ))}
         </Picker>
-        {pokemon?<Text>voce selecionou {pokemon}</Text>:''}
+        {pokemon?<Text>Você selecionou {pokemon}</Text>:''}
         </View>
     )
 }
